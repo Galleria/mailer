6adf42d3c6c0d346448b9ca7c810be4e68fcb3c0
Sulu <cadet@Saaviks-Mac-mini.local>
2016-04-26 09:58:43 +0700

    Initial spring boot

---
+++ src/main/java/com/main/SampleController.java
@@ -0,0 +1,23 @@
+package com.main;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+/**
+ * Created by cadet on 4/26/2016 AD.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
