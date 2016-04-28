1fb5bb12a4b7695ae319cdbaa05ec44acf469399
bright <udom.kasemosoth@gmail.com>
2016-04-28 09:59:59 +0700

    Send real email

---
+++ src/main/java/com/main/entities/EmailForm.java
@@ -0,0 +1,34 @@
+package com.main.entities;
+
+/**
+ * Created by cadet on 4/27/2016 AD.
+ */
+public class EmailForm {
+    private String to;
+    private String topic;
+    private String body;
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
